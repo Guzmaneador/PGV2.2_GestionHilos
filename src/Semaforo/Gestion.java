1141920d33519d993a45b5d34daad12cdd786ee6
munchi <munchi@munchi-PC>
2019-10-29 16:29:11 +0000

    ->Finalizado y comentado

--- src/Semaforo/Gestion.java
+++ src/Semaforo/Gestion.java
@@ -12,7 +12,7 @@ public class Gestion {
 
     public Gestion() {
         listaCoches= new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 150; i++) {
             listaCoches.add(new Coche(i,parking));   
         }
         abreElParking();
@@ -28,14 +28,5 @@ public class Gestion {
   
     }
 
-    public static Parking getParking() {
-        return parking;
-    }
-
-    public static void setParking(Parking parking) {
-        Gestion.parking = parking;
-    }
-    
-    
 
 }
--- src/Semaforo/Parking.java
+++ src/Semaforo/Parking.java
@@ -11,6 +11,7 @@ public class Parking {
     int numeroPlazas;
     int numeroPlazasOcupadas;
     boolean completo;
+    //Se crea el objeto de tipo Semaphore
     Semaphore semaforoEntrada;
 
 
@@ -19,6 +20,7 @@ public class Parking {
         numeroPlazas=115;
         completo=false;
         numeroPlazasOcupadas=0;
+        //Creamos el semaforo de tipo binario
         semaforoEntrada = new Semaphore(1);
 
 
@@ -26,26 +28,26 @@ public class Parking {
     
     public void entraCoche() throws InterruptedException{
         while(completo){
-            semaforoEntrada.acquire();
+            //El semaforo podra su bit a cero si completo es true.
+                semaforoEntrada.acquire();
+
         }
             numeroPlazasOcupadas++;
 
             if(numeroPlazas==numeroPlazasOcupadas){
                 completo = true;
                 System.out.println("Parking completo, no se aceptan mas coches.");
+
             }
-            
-            
-        
-        
-        
-     
     }
     public void saleCoche(){
         if(completo){
             completo = false;
             numeroPlazasOcupadas--;
+            //El semaforo podra el bit a uno cuando se libre un plaza estando completo = true.
             semaforoEntrada.release();
+            
+
 
         }else{
             if(numeroPlazasOcupadas !=0){
